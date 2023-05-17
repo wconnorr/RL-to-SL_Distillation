6efe91889778e70f8c14b9f8dca8d1e1d0ff004b
Connor Wilhelm <wilhelmconnorr@gmail.com>
2023-05-17 15:47:40 -0600

    Added basic args and torch device checking to main.py. Filled in Atari and cart-pole env names in envs/envs.json.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Imports
+import json
 import torch
 
 from envs import vector_env
 from argparse import ArgumentParser
 
 def main():
-  atari_envs = []
-  cartpole_envs = []
+  with open('envs/envs.json', 'r') as json_f:
+    env_names = json.loads(json_f.read())
+  atari_envs = env_names['atari']
+  cartpole_envs = env_names['cartpole']
 
   # All arguments from terminal
   parser = ArgumentParser()
@@ -14,7 +17,10 @@ def main():
   parser.add_argument('--rl', help='Perform RL learning or validation (no distillation).')
   parser.add_argument('--train', help='Perform training. If "--rl", performs standard RL training with a single architecture; else, performs distillation training.', action='store_true')
   parser.add_argument("--validate", help='Perform validation. If "--train", validation is performed after training is completed; otherwise, validation is performed using pretrained or randomly-initialized models.', action='store_true')
+  parser.add_argument('-b', '--inner_batch_size', help='Size of inner batch: i.e. number of data instances created by the distiller.', type=int, default=512)
+  parser.add_argument('-d', '--device', help="String for `torch.device` indicating which device to use for tensor computation. Defaults to cpu.", default='cpu')
   parser.add_argument("-e","--encoder", help="Layer at which to separate Actor architecture into Encoder [0,e) and Actor [e,L). If not specified, encoder will not be used. Accepts positive and negative values used as indexes into the architecture. If 0 or unspecified, no encoder is created", type=int)
+  parser.add_argument('--inner_lr', help='Initial learning rate for inner optimization. This will be learned through meta-learning as a parameter in the distiller.', type=float, default=2e-2)
   # Load pretrained models / model initializations
   parser.add_argument("--load_distiller", help='Path to state dictionary file to initialize distiller. If "--rl", distiller is used to pretrain actor before RL learning.')
   parser.add_argument("--load_actor", help='Path to state dictionary file to initialize actor.')
@@ -34,12 +40,29 @@ def main():
   args = parser.parse_args()
   env_name = args.environment
   use_atari = env_name in atari_envs
+  use_cartpole = env_name in cartpole_envs
 
   # Argument-dependent local imports
   if use_atari:
     from models import atari_models as models
-  else:
+  elif use_cartpole:
     from models import cartpole_models as models
+  else:
+    raise ValueError("Environment {} not recognized. If the environment is a valid gym environment, add it to the appropriate list in ./envs/envs.json.".format(env_name))
+
+  # Select device
+  device = torch.device(args.device)
+  if device.type == 'cuda':
+    try:
+      assert torch.cuda.is_available()
+    except AssertionError:
+      print("ERROR: Cannot select device {}: CUDA devices are currently unavailable.".format(device))
+      exit()
+  elif device.type != 'cpu':
+    try:
+      torch.Tensor([]).to(device)
+    except Exception:
+      print("ERROR: Cannot allocate tensor on device {}; device may be unavailable.".format(device))
 
   # Get state/action space
   env = vector_env.make_env(env_name)
@@ -48,7 +71,7 @@ def main():
   del env # we don't need the environment anymore, we just need the shapes
 
   # Create or load requisite models
-  distiller = models.Distiller(*state_size, action_size, batch_size, init_lr)
+  distiller = models.Distiller(*state_size, action_size, args.inner_batch_size, args.inner_lr)
   # TODO: Encoder
   if args.encoder:
     actor, encoder = models.create_encoder_actor(state_size[0], action_size, args.encoder)
